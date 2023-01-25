e14edaffa7e8c5c4bdcfbd19bc0eab9fa2d061c2
Nikita Park <antoniodionis4@gmail.com>
2023-01-25 18:40:49 +0300

    fixed unused import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
-import Main from "./components/Main";
+
 
 import "./styles/main.css";
 
