8dbbb84464ae61d7b0b59818784a6114b7b2b2ac
Nikita Park <antoniodionis4@gmail.com>
2023-02-21 16:33:24 +0300

    added noindex

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import AppRoutes from "./AppRoutes";
+import { Helmet } from "react-helmet";
 
 const App = () => (
     <div className="container">
+        <Helmet><meta name="robots" content="noindex"/></Helmet>
         <AppRoutes />
     </div>
 );
